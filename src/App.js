7bcb3e887f5b67a3bb8cf13ed668b7d44324cbc2
AlexanderDLe <iamxanofficial@gmail.com>
2018-08-04 01:05:48 -0700

    Browser Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Landing from './components/pages/Landing';
 import Portfolio from './components/pages/Portfolio';
@@ -8,12 +8,12 @@ import Portfolio from './components/pages/Portfolio';
 class App extends Component {
   render() {
     return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div className="App">
           <Route exact path="/" component={Landing} />
           <Route exact path="/portfolio" component={Portfolio} />
         </div>
-      </Router>
+      </BrowserRouter>
     );
   }
 }
