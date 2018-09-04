e8f27d36fffc2d0e5ddf08eba1fcd41d24f6783c
AlexanderDLe <iamxanofficial@gmail.com>
2018-09-03 21:15:14 -0700

    Portfolio pages and button radius.

--- src/components/home/portfolio/Lambada.js
+++ src/components/home/portfolio/Lambada.js
@@ -1,32 +1,30 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Container } from 'reactstrap';
 import Lambadaimg from '../../../img/Lambada Site.png';
 import MyButton from '../../utility/MyButton';
 
 export default () => {
   return (
-    <div className="full-page greenBG portfolio-container">
-      <Container className="test">
-        <Row className="portfolio-container">
-          <Col md="12" xl="6" className="py-5 center-items">
-            <h1 className="OpenSans page-title text-center py-1">Lambada</h1>
-            <p className="lead">
-              A bright and simplistic site for a welcoming boba shop.
-            </p>
-            <p className="text-muted">
-              This friendly page features a full menu gallery, a sliding image
-              carousel, information, and nice vibes. Fully responsive, quick,
-              and easy to get the info you need - it's perfect for a boba shop.
-            </p>
-            <div className="m-2 row">
-              <MyButton to={'/portfolio'} color={'black'} name={'Website'} />
-              <MyButton to={'/portfolio'} color={'black'} name={'Code'} />
-            </div>
-          </Col>
-          <Col>
-            <img src={Lambadaimg} className="portfolio-icon" alt="" />
-          </Col>
-        </Row>
+    <div className="full-portfolio-page greenBG portfolio-container">
+      <Container className="portfolio-reverse-page">
+        <div className="flex-item reverse-item py-5 center-items portfolio-text">
+          <h1 className="OpenSans page-title text-center py-1">Lambada</h1>
+          <p className="lead">
+            A bright and simplistic site for a welcoming boba shop.
+          </p>
+          <p className="text-muted">
+            This friendly page features a full menu gallery, a sliding image
+            carousel, information, and nice vibes. Fully responsive, quick, and
+            easy to get the info you need - it's perfect for a boba shop.
+          </p>
+          <div className="m-2 row">
+            <MyButton to={'/portfolio'} color={'black'} name={'Website'} />
+            <MyButton to={'/portfolio'} color={'black'} name={'Code'} />
+          </div>
+        </div>
+        <div className="flex-item">
+          <img src={Lambadaimg} className="portfolio-icon" alt="" />
+        </div>
       </Container>
     </div>
   );
--- src/components/home/portfolio/VVN.js
+++ src/components/home/portfolio/VVN.js
@@ -1,31 +1,29 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Container } from 'reactstrap';
 import VVNimg from '../../../img/VVN Site.png';
 import MyButton from '../../utility/MyButton';
 export default () => {
   return (
-    <div className="full-page blueBG portfolio-container">
-      <Container className="test">
-        <Row className="portfolio-container">
-          <Col>
-            <img src={VVNimg} className="portfolio-icon" alt="" />
-          </Col>
-          <Col md="12" xl="6" className="py-5 center-items">
-            <h1 className="OpenSans page-title text-center py-1">VVN</h1>
-            <p className="lead">
-              A custom site developed for electronic artist VVN.
-            </p>
-            <p className="text-muted">
-              This page features music, services, information, and artwork
-              belonging to VVN. Responsive, detailed, and stylish - it perfectly
-              suits her unique identity. Check it out.
-            </p>
-            <div className="m-2 row">
-              <MyButton to={'/portfolio'} color={'black'} name={'Website'} />
-              <MyButton to={'/portfolio'} color={'black'} name={'Code'} />
-            </div>
-          </Col>
-        </Row>
+    <div className="full-portfolio-page blueBG portfolio-container">
+      <Container className="portfolio-page">
+        <div className="flex-item">
+          <img src={VVNimg} className="portfolio-icon" alt="" />
+        </div>
+        <div className="py-5 center-items flex-item portfolio-text">
+          <h1 className="OpenSans page-title text-center py-1">VVN</h1>
+          <p className="lead">
+            A custom site developed for electronic artist VVN.
+          </p>
+          <p className="text-muted">
+            This page features music, services, information, and artwork
+            belonging to VVN. Responsive, detailed, and stylish - it perfectly
+            suits her unique identity. Check it out.
+          </p>
+          <div className="m-2 row">
+            <MyButton to={'/portfolio'} color={'black'} name={'Website'} />
+            <MyButton to={'/portfolio'} color={'black'} name={'Code'} />
+          </div>
+        </div>
       </Container>
     </div>
   );
