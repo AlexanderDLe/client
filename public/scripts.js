506fca025e3a32602409b3fca7137084bb49e342
AlexanderDLe <iamxanofficial@gmail.com>
2018-09-04 21:00:00 -0700

    Nav Icon Color Adaption!

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,4 @@
-// Parallax FX
+// Scroll FX
 window.addEventListener('scroll', function() {
   if (document.querySelector('.parallaxFX')) {
     let parallaxFX = document.querySelector('.parallaxFX');
@@ -7,7 +7,7 @@ window.addEventListener('scroll', function() {
 });
 
 // Toggle Nav
-const navIcon = document.querySelector('.nav-icon');
-navIcon.addEventListener('click', function() {
-  navIcon.classList.toggle('active');
-});
+// const navIcon = document.querySelector('.nav-icon');
+// navIcon.addEventListener('click', function() {
+//   navIcon.classList.toggle('active');
+// });
--- src/components/Nav/Navigation.js
+++ src/components/Nav/Navigation.js
@@ -1,14 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 
-export default () => {
-  return (
-    <div className="navigation">
-      <Container className="nav-container">
-        <div className="nav-icon">
-          <div className="hamburger" />
-        </div>
-      </Container>
-    </div>
-  );
-};
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+export default class Nav extends Component {
+  constructor() {
+    super();
+    this.state = {
+      toggle: false,
+      iconColor: ''
+    };
+    this.toggleOnClick = this.toggleOnClick.bind(this);
+    this.handleScroll = debounce(this.handleScroll.bind(this), 15);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+    this.handleScroll();
+  }
+
+  handleScroll(e) {
+    if (window.scrollY > window.innerHeight - 16) {
+      this.setState({
+        iconColor: 'nav-black'
+      });
+    } else if (window.scrollY < window.innerHeight - 16) {
+      this.setState({
+        iconColor: ''
+      });
+    }
+  }
+
+  toggleOnClick() {
+    if (!this.state.toggle) {
+      this.setState({
+        toggle: 'active'
+      });
+    }
+    if (this.state.toggle) {
+      this.setState({
+        toggle: false
+      });
+    }
+  }
+
+  render() {
+    const { toggle, iconColor } = this.state;
+
+    return (
+      <div className="navigation">
+        <Container className="nav-container">
+          <div
+            onClick={this.toggleOnClick}
+            className={`nav-icon ${toggle} ${iconColor}`}
+          >
+            <div className="hamburger" />
+          </div>
+        </Container>
+      </div>
+    );
+  }
+}
