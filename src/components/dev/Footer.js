033e84ecde43ad6cf6d5a46fe073f76cc40d944f
AlexanderDLe <iamxanofficial@gmail.com>
2018-10-28 19:44:50 -0700

    Removed IG link in Dev Page

--- src/components/dev/Footer.js
+++ src/components/dev/Footer.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import GHicon from '../../img/GH Icon.png';
 import LIicon from '../../img/LI Icon.png';
-import IGicon from '../../img/IG Icon.png';
 import CPicon from '../../img/CP Icon.png';
 import OpenAnchorTag from '../utility/OpenAnchorTag';
 
 export default () => {
   return (
     <footer className="footer-portfolio center-items-horizontally">
-      <OpenAnchorTag
-        img={IGicon}
-        tag="footer"
-        href="https://www.instagram.com/xan.stagram/"
-      />
       <OpenAnchorTag
         img={LIicon}
         tag="footer"
